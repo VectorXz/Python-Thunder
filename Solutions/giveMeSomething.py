62de619843cf2e27d71808a4a0759e8efcbb740d
vectorxz <vectorx2263@hotmail.com>
2020-10-06 21:57:49 +0700

    add returnSomethingToMe.py

---
+++ Solutions/giveMeSomething.py
@@ -0,0 +1,16 @@
+"""
+Problem statement:
+Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+Examples
+give_me_something("is better than nothing") ➞ "something is better than nothing"
+
+give_me_something("Bob Jane") ➞ "something Bob Jane"
+
+give_me_something("something") ➞ "something something"
+
+Problem Link: https://edabit.com/challenge/XXJbGFEkrMWCp8yFn
+"""
+
+def give_me_something(a):
+	return "something "+a
